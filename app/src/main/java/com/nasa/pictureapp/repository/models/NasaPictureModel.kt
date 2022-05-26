91881b188a70b55d8347191c443b07b893a326ed
Raj Turakhia <rturakhia007@gmail.com>
2022-05-26 15:48:59 +0530

    Completed Home screen UI design and data binding.

---
+++ app/src/main/java/com/nasa/pictureapp/repository/models/NasaPictureModel.kt
@@ -0,0 +1,23 @@
+package com.nasa.pictureapp.repository.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class NasaPictureModel(
+    @SerializedName("copyright")
+    val copyright: String? = null,
+    @SerializedName("date")
+    val date: String? = null,
+    @SerializedName("explanation")
+    val explanation: String? = null,
+    @SerializedName("hdurl")
+    val hdurl: String? = null,
+    @SerializedName("media_type")
+    val mediaType: String? = null,
+    @SerializedName("service_version")
+    val serviceVersion: String? = null,
+    @SerializedName("title")
+    val title: String? = null,
+    @SerializedName("url")
+    val url: String? = null
+)
